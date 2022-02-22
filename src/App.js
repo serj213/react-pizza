51678a0142e5f6c5b291d85f784d4ccb14819623
Sergey <kornilovsergey55@gmail.com>
2022-02-22 23:17:27 +0300

    hooks redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { setPizzas, setLoaded } from './redux/actions/pizzas';
 
 import Header from './Components/Header/Header';
@@ -9,60 +9,52 @@ import Loader from './Components/Loader/Loader';
 
 
 
-class App extends React.Component {
+const App = props => {
 
+  const dispatch = useDispatch();
+  const state = useSelector(({ pizzas }) => {
+    return {
+      items: pizzas.items,
+      isLoaded: pizzas.isLoaded
+    }
+  })
 
-  componentDidMount() {
-
+  React.useEffect(() => {
     axios.get('http://localhost:3001/pizzas').then(({ data }) => {
-      this.props.setPizzasGet(data)
+      dispatch(setPizzas(data))
     }).finally(() => {
-      this.props.changeLoaded(false);
-      console.log('dfdf');
+      dispatch(setLoaded(false));
     })
-  }
+  }, [])
 
-  render() {
+  return (
+    <div className="wrapper">
+      {
+        state.isLoaded ? <Loader />
+          :
+          <>
+            <Header />
+            <Home items={state.items} />
+          </>
+      }
 
-    console.log('this.props.isLoaded', this.props.isLoaded);
-
-    return (
-      <>
-        <div className="wrapper">
-
-          {
-            this.props.isLoaded ? <Loader />
-              :
-              <>
-                <Header />
-                <Home items={this.props.items} />
-              </>
-          }
-
-
-
-        </div>
-
-      </>
-    )
-  }
-}
-
-const mapStateToProps = state => {
-  return {
-    items: state.pizzas.items,
-    isLoaded: state.pizzas.isLoaded
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    setPizzasGet: (items) => dispatch(setPizzas(items)),
-    changeLoaded: (boolean) => dispatch(setLoaded(boolean))
-  }
+    </div>
+  )
 }
 
 
 
+export default App;
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// export default connect(
+//   (state) => ({
+//     items: state.pizzas.items,
+//     isLoaded: state.pizzas.isLoaded
+//   }),
+//   (dispatch) => ({
+//     setPizzasGet: (items) => dispatch(setPizzas(items)),
+//     changeLoaded: (boolean) => dispatch(setLoaded(boolean))
+//   })
+
+// )(App);
